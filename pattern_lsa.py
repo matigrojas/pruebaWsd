7f52d37268d5da10e02cb3c832b6f95c8e0f6e8a
matigrojas <rojasmatias994@gmail.com>
2018-04-09 15:42:12 -0300

    07042018

--- pattern_lsa.py
+++ pattern_lsa.py
@@ -6,12 +6,14 @@ d3 = Document('The dog wags his tail.', name='dog1')
 d4 = Document('The dog is happy.', name='dog2')
 
 m = Model([d1, d2, d3, d4])
-m.reduce(2)
+m.reduce(4)
  
 for d in m.documents:
     print
     print d.name
     for concept, w1 in m.lsa.vectors[d.id].items():
+        print m.lsa.vectors[d.id].items() #matriz U
         for feature, w2 in m.lsa.concepts[concept].items():
+            #print m.lsa.concepts[concept].items() #matriz Vt
             if w1!=0 and w2!=0:
                 print (feature, w1 * w2)
